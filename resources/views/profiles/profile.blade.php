2f96555665878178818bc58b5ba6e63f786344fc
Jeremy Chen <clipclop182@gmail.com>
2017-05-01 10:17:43 +0800

    display user's profile info

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -12,11 +12,30 @@
                 </div>
                 <div class="panel-body">
                     <img class="center-block img-circle img-thumbnail" src="{{ Storage::url($user->avatar) }}" width="70px" height="70px">
+                    <br>
+                    <p class="text-center">
+                        {{ $user->profile->location }}
+                    </p>   
+                    <br>
                     <p class="text-center">
                         @if(Auth::id() == $user->id)
                             <a href="{{ route('profile.edit')}}" class="btn btn-lg btn-info">Edit your profile</a>
 
                         @endif
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <p class="text-center">
+                        About me
+                    </p>
+                </div>
+                <div class="panel-body">
+                    <p class="text-center">
+                        {{ $user->profile->about}}
                     </p>
                 </div>
             </div>
