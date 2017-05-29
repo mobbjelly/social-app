4a1f60939b7ac907af20d86ba06b0f75ee802db8
Jeremy Chen <clipclop182@gmail.com>
2017-05-29 20:41:23 +0800

    create Feed.vue component

---
+++ resources/assets/js/components/Feed.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-lg-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">
+                        Jeremy Chen
+                    </div>
+
+                    <div class="panel-body">
+                        <p class="text-center">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ex fugiat laboriosam omnis, porro quidem ratione repellat rerum sunt voluptatibus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
