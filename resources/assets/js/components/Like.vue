3cfaeaf70fa28d0e60bd772d07c05319f1bb28eb
Jeremy Chen <clipclop182@gmail.com>
2017-08-19 09:35:56 +0800

    displaying post likes

--- resources/assets/js/components/Like.vue
+++ resources/assets/js/components/Like.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <hr>
-    <p class="text-center" v-for="like in post.likes">
+    <span class="text-center" v-for="like in post.likes">
       <img :src="like.user.avatar" alt="" width="40px" height="40px" class="avatar-likes">
-    </p>
+    </span>
     <hr>
-    <button class="btn btn-primary">
+    <button class="btn btn-primary" v-if="!auth_user_likes_post">
       Like this post
     </button>
-    <button class="btn btn-danger">
+    <button class="btn btn-danger" v-else>
       Unlike this post
     </button>
   </div>
@@ -22,6 +22,28 @@
     },
     props: ["id"],
     computed: {
+      likers() {
+        let likers = []
+
+        this.post.likes.forEach(like => {
+          likers.push(like.user.id)
+        })
+
+        return likers
+      },
+
+      auth_user_likes_post() {
+        let check_index = this.likers.indexOf(
+          this.$store.state.auth_user.id
+        )
+
+        if (check_index === -1) {
+          return false
+        } else {
+          return true
+        }
+      },
+
       post() {
         return this.$store.state.posts.find(post => {
           return post.id === this.id
